e389370113365f6d6124a89dc58f0d3a17fbc0ff
Brandon <61928787+btwitsmyname@users.noreply.github.com>
2025-02-17 08:42:20 -0500

    Add files via upload

--- AI-assistant (small).py
+++ AI-assistant (small).py
@@ -1,3 +1,4 @@
+#! /usr/bin/env/Python3
 #import all libaries
 import pyttsx3
 import vosk
@@ -10,18 +11,14 @@ engine = pyttsx3.init()
 def start_general():
     #set path to language model
     model = vosk.Model("vosk-model-small-en-us-0.15")
-    #set recorder and standard samplerate for audio files
+    #set recorder and standard sample rate for audio files
     rec = vosk.KaldiRecognizer(model,16000)
     #set up mic
     p = pyaudio.PyAudio()
-    stream = p.open(format=pyaudio.paInt16,
-                    channels = 1,
-                    rate = 16000,
-                    input = True,
-                    frames_per_buffer = 8192)
+    stream = p.open(format=pyaudio.paInt16,channels = 1, rate = 16000,
+                    input = True, frames_per_buffer = 8192)
     #set output path
-    output_file_path = "prompt.txt"
-    with open(output_file_path, "w") as output_file:
+    with open("prompt.txt", "w") as output_file:
         text = "What is your question"
         print (text)
         engine.say(text)
@@ -38,29 +35,35 @@ def start_general():
                 print (prompt)
                 if "cancel" in prompt.lower():
                     text = "action canceled"
+                    break
                 #push prompt to ollama model
-                else: 
-                    text = ollama.generate(model='llama3.2',prompt=prompt)
-                print (text)
-                engine.say(text)
-                time.sleep(0.5)
-                engine.runAndWait()
-                break
+                else:
+                    #make a file for the response
+                    file = open("response.txt","a")
+                    #generate the ollama response
+                    response =generation = ollama.chat(model='llama3.2',messages=[{'role':'user','content':prompt,},])
+                    print ("Generating response")
+                    engine.say("Generating response")
+                    time.sleep(0.5)
+                    #variable for the response
+                    text = (response['message']['content'])
+                    #text = ollama.generate(model='llama3.2',prompt=prompt)
+                    print (text)
+                    engine.say(text)
+                    time.sleep(30)
+                    engine.runAndWait()
+                    break
 def start_math():
     #set path to language model
     model = vosk.Model("vosk-model-small-en-us-0.15")
-    #set recorder and standard samplerate for audio files
+    #set recorder and standard sample rate for audio files
     rec = vosk.KaldiRecognizer(model,16000)
     #set up mic
     p = pyaudio.PyAudio()
-    stream = p.open(format=pyaudio.paInt16,
-                    channels = 1,
-                    rate = 16000,
-                    input = True,
-                    frames_per_buffer = 8192)
+    stream = p.open(format=pyaudio.paInt16,channels = 1, rate = 16000,
+                    input = True, frames_per_buffer = 8192)
     #set output path
-    output_file_path = "prompt.txt"
-    with open(output_file_path, "w") as output_file:
+    with open("prompt.txt", "w") as output_file:
         text = "What is your equation"
         print (text)
         engine.say(text)
@@ -77,14 +80,24 @@ def start_math():
                 print (prompt)
                 if "cancel" in prompt.lower():
                     text = "action canceled"
+                    break
                 #push prompt to ollama model
                 else:
-                    text = ollama.generate(model='qwen2-math',prompt=prompt)
-                print (text)
-                engine.say(text)
-                time.sleep(0.5)
-                engine.runAndWait()
-                break
+                    #make a file for the response
+                    file = open("response.txt","a")
+                    #generate the ollama response
+                    response =generation = ollama.chat(model='qwen2-math',messages=[{'role':'user','content':prompt,},])
+                    print ("Generating response")
+                    engine.say("Generating response")
+                    time.sleep(0.5)
+                    #variable for the response
+                    text = (response['message']['content'])
+                    #text = ollama.generate(model='llama3.2',prompt=prompt)
+                    print (text)
+                    engine.say(text)
+                    time.sleep(30)
+                    engine.runAndWait()
+                    break
 #a function to call and use the stt function
 def start_stt():
     #set path to language model
